cb9f2392d891091cb95500a1b58f7a4527beaf72
Otilia Toma <otilia@192-168-0-132.rdsnet.ro>
2023-12-19 22:44:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+import React from 'react'
+import Navbar from "./components/Navbar"
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            
+            <img src="/images/logo--navbar.png" className="nav--logo" />
+            <h1 className="h1--logo">my travel journal - 2023</h1>
+        </nav>
+    )
+}
